a4afa4a8a6c0c08c8dbd3736b2274201e65166a7
Ricardo Mello <ricardohsmello@gmail.com>
2026-02-15 20:09:38 -0300

    Removing unnecessary classes

--- src/main/java/com/devnexus/frauddetection/infrastructure/embedding/config/EmbeddingConfig.java
+++ src/main/java/com/devnexus/frauddetection/infrastructure/embedding/config/EmbeddingConfig.java
@@ -1,8 +1,6 @@
 package com.devnexus.frauddetection.infrastructure.embedding.config;
 
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableConfigurationProperties(VoyageEmbeddingProperties.class)
-class EmbeddingConfig {}
+//
+//@Configuration
+//@EnableConfigurationProperties(VoyageEmbeddingProperties.class)
+//class EmbeddingConfig {}
