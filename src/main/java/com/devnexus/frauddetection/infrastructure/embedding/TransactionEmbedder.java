e66532eaab753782f366cfb340e94af2286e6892
Ricardo Mello <ricardohsmello@gmail.com>
2026-02-15 20:09:25 -0300

    Removing unnecessary classes

--- src/main/java/com/devnexus/frauddetection/infrastructure/embedding/TransactionEmbedder.java
+++
@@ -1,9 +0,0 @@
-package com.devnexus.frauddetection.infrastructure.embedding;
-
-import com.devnexus.frauddetection.domain.model.Transaction;
-import org.springframework.data.domain.Vector;
-
-
-public interface TransactionEmbedder {
-    Vector embed(Transaction transaction);
-}
